0240bb961bcd0e9d9f573bdd8d6716be6232c121
Server <november14giel@gmail.com>
2018-04-16 20:42:52 -0400

    files

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// const api = require("./server/routes/api");
+// app.use(api, "/api")
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static(path.join(__dirname, "./eventmanager/dist")));
+
+// Angular path location strategy
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./eventmanager/dist/index.html"));
+});
+// Set up promises with mongoose
+// mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+//   {
+//     // useMongoClient: true
+//   }
+// );
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
